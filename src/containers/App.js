cb997b9a3aa9811900112820eecdf75e5dd48618
mrmrs <hi@mrmrs.cc>
2016-06-01 23:10:15 +0100

    Update copy

--- src/containers/App.js
+++ src/containers/App.js
@@ -28,7 +28,7 @@ const App = React.createClass({
         </div>
         <header className='bg-white pa4 pv5-l'>
           <h1 className='f4 f3-m f1-l mt0 mb3 fw6 tc'><span className=''>Random A11y Color Palettes</span></h1>
-          <p className='gray f5  mt0 tc'>There are many a11y compliant color palettes. But which ones looks good?</p>
+          <p className='gray f5  mt0 tc'>There are many a11y compliant color palettes. Which ones look good?</p>
         </header>
         <section className='cf bg-white'>
           <div className='fl w-100 w-50-ns pv4 ph4' style={{ backgroundColor: colorOne, color: colorTwo }}>
