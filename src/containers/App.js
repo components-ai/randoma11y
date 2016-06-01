73e344d39000e8ebaee1754f4c5fd28e9a23872c
mrmrs <hi@mrmrs.cc>
2016-06-01 21:14:45 +0100

    Bump tachyons number. Tweak site styles

--- src/containers/App.js
+++ src/containers/App.js
@@ -22,7 +22,10 @@ const App = React.createClass({
     const colorTwo = colors && colors[1]
 
     return (
-      <div className='sans-serif'>
+      <div className='sans-serif relative'>
+        <div className='bg-black near-white tc pv2 fixed left-0 top-0 right-0'>
+          <p className='fw6 ttu tracked mv0'>Success!</p>
+        </div>
         <header className='bg-white pa4 pv5-l'>
           <h1 className='f4 f3-m f1-l mt0 mb3 fw6 tc'><span className=''>Random A11y Color Palettes</span></h1>
           <p className='gray f5  mt0 tc'>There are many a11y compliant color palettes. But which ones looks good?</p>
@@ -30,39 +33,39 @@ const App = React.createClass({
         <section className='cf bg-white'>
           <div className='fl w-100 w-50-ns pv4 ph4' style={{ backgroundColor: colorOne, color: colorTwo }}>
             <code className='f4 f3-ns mb2 mb5-ns db'>{colorOne}</code>
-            <h3 className="f1 f-headline-l mv0">Aa</h3>
+            <h3 className='f1 f-headline-l mv0'>Aa</h3>
             <p className='lh-copy measure'>
               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-              tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-              vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-              no sea takimata sanctus est Lorem ipsum dolor sit amet.
+              tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
             </p>
           </div>
           <div className='fl w-100 w-50-ns pv4 ph4' style={{ backgroundColor: colorTwo, color: colorOne }}>
             <code className='f4 f3-ns mb2 mb5-ns db'>{colorTwo}</code>
-            <h3 className="f1 f-headline-l mv0">Aa</h3>
+            <h3 className='f1 f-headline-l mv0'>Aa</h3>
             <p className='lh-copy measure'>
               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-              tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-              vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-              no sea takimata sanctus est Lorem ipsum dolor sit amet.
+              tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
             </p>
           </div>
         </section>
         <div className='pa4 cf bg-white'>
           <div className='w-100 tc'>
-            <button className='input-reset black ba b--black-20 ph3 ph4-ns pv2 pv3-ns br2 dim bg-white fw6 ttu f6 tracked mr3' >Vote Up</button>
-            <button className='input-reset black ba b--black-20 ph3 ph4-ns pv2 pv3-ns br2 dim bg-white fw6 ttu f6 tracked mr3' >Vote Down</button>
-            <button className='input-reset black ba b--black-20 ph3 ph4-ns pv2 pv3-ns br2 dim bg-white fw6 ttu f6 tracked' onClick={this.handleNewColorsClick}>Next</button>
+            <button className='bg-hover-near-white input-reset black ba b--black-20 ph3 ph4-ns pv2 pv3-ns br2 bg-white fw6 ttu f6 tracked mr3 cursor' >Vote Up</button>
+            <button className='input-reset black ba b--black-20 ph3 ph4-ns pv2 pv3-ns br2 dim bg-white fw6 ttu f6 tracked mr3 cursor' >Vote Down</button>
+            <button className='input-reset black ba b--black-20 ph3 ph4-ns pv2 pv3-ns br2 dim bg-white fw6 ttu f6 tracked cursor' onClick={this.handleNewColorsClick}>Next</button>
             <div className='tc'>
-              <p className='measure dib  fw1 black-70'>
+              <p className='measure fw1 center'>
                 The more you vote, the faster the robots learn.
               </p>
             </div>
           </div>
         </div>
-        <footer className='ph4 pv4 bt b--black-10 '>
-          Built by <span className='fw6'>@4lpine</span> and <span className='fw6'>@mrmrs_</span>
+        <footer className='ph4 pt4 pb5 bt b--black-10 '>
+          <p className='mb3'>Built by <span className='fw6'>@4lpine</span> and <span className='fw6'>@mrmrs_</span></p>
+
+          <div className='pt3'>
+      <a href='https://twitter.com/share' className='f6 ttu fw6 link twitter-share-button bg-blue white br1 ph3 pv2' data-via='mrmrs_' data-size='large'>Tweet This</a>
+      </div>
         </footer>
       </div>
     )
