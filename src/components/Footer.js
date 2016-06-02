31625af7bb1568854a89ed0f284b6424a3b4517a
John Otander <johnotander@gmail.com>
2016-06-02 13:50:08 -0600

    A typo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ const Footer = () => {
       <div className='pt3 cf'>
   </div>
       <p className='mt5 gray'>
-        Related projects: <a href='//jxnblk.com/colorable' title='a11y contrast checker' className='link dim black fw6'>Colorable</a> <a href='//clrs.cc' title='A nicer color palette for the web.' className='dib ml2 link dim black fw6'>Colors</a> <a href='https://github.com/johnotander/random-a11y-combo' title='random a11y combo generator' className='dib ml2 link dim black fw6'>random-a11y-combo</a> <a href='https://github.com/johnotander/get-contrast' ti    tle='get contrast ratios and scores' className='dib ml2 link dim black fw6'>get-contrast</a>
+        Related projects: <a href='//jxnblk.com/colorable' title='a11y contrast checker' className='link dim black fw6'>Colorable</a> <a href='//clrs.cc' title='A nicer color palette for the web.' className='dib ml2 link dim black fw6'>Colors</a> <a href='https://github.com/johnotander/random-a11y-combo' title='random a11y combo generator' className='dib ml2 link dim black fw6'>random-a11y-combo</a> <a href='https://github.com/johnotander/get-contrast' title='get contrast ratios and scores' className='dib ml2 link dim black fw6'>get-contrast</a>
       </p>
     </footer>
   )
