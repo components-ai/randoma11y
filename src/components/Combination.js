b2a1d6b81e1f893761d8430cf979428a696775b9
mrmrs <hi@mrmrs.cc>
2016-06-02 20:03:28 +0100

    Remove redundant labels

--- src/components/Combination.js
+++ src/components/Combination.js
@@ -6,8 +6,8 @@ const Combination = ({ colorOne, colorTwo }) => {
     <section className='cf bg-white'>
       <div className='fl w-100 w-50-ns pv4 ph4' style={{ backgroundColor: colorOne, color: colorTwo }}>
         <code className='f4 mb2 db fw6'>{colorOne}</code>
-        <code className='f5 mb2 db'>hsla: {Color(colorOne).hslString()}</code>
-        <code className='f5 mb2 mb4-ns db'>rgb: {Color(colorOne).rgbString()}</code>
+        <code className='f5 mb2 db'>{Color(colorOne).hslString()}</code>
+        <code className='f5 mb2 mb4-ns db'>{Color(colorOne).rgbString()}</code>
         <h3 className='f1 f-headline-l mv0'>Aa Bb Cc</h3>
         <p className='lh-copy measure'>
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
@@ -16,8 +16,8 @@ const Combination = ({ colorOne, colorTwo }) => {
       </div>
       <div className='fl w-100 w-50-ns pv4 ph4' style={{ backgroundColor: colorTwo, color: colorOne }}>
         <code className='f4 fw6 mb2 db'>{colorTwo}</code>
-        <code className='f5 mb2 db'>hsla: {Color(colorTwo).hslString()}</code>
-        <code className='f5 mb2 mb4-ns db'>rgb: {Color(colorTwo).rgbString()}</code>
+        <code className='f5 mb2 db'>{Color(colorTwo).hslString()}</code>
+        <code className='f5 mb2 mb4-ns db'>{Color(colorTwo).rgbString()}</code>
         <h3 className='f1 f-headline-l mv0'>Aa Bb Cc</h3>
         <p className='lh-copy measure'>
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
