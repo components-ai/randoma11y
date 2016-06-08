21f775a2639465537d396d5a20f0a0bcf6742a39
mrmrs <hi@mrmrs.cc>
2016-06-08 17:02:56 +0100

    Better voting across breakpoints.

--- src/components/Combination.js
+++ src/components/Combination.js
@@ -5,23 +5,23 @@ const Combination = ({ colorOne, colorTwo }) => {
   return (
     <section className='cf bg-white'>
       <div className='fl w-100 w-50-ns pv4 ph4' style={{ backgroundColor: colorOne, color: colorTwo }}>
-        <code className='f4 mb2 db fw6'>{colorOne}</code>
-        <code className='f5 mb2 db'>{Color(colorOne).hslString()}</code>
-        <code className='f5 mb2 mb4-ns db'>{Color(colorOne).rgbString()}</code>
-        <h3 className='f1 f-headline-l mv0'>Aa Bb Cc</h3>
+        <code className='f6 f4-ns mb2 db fw6'>{colorOne}</code>
+        <code className='f6 f5-ns mb2 db'>{Color(colorOne).hslString()}</code>
+        <code className='f6 f5-ns mb2 mb4-ns db'>{Color(colorOne).rgbString()}</code>
+        <h3 className='f3 f1-m f-headline-l mv0'>Aa Bb Cc</h3>
         <p className='lh-copy measure'>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-          tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.
+          <span className='dn di-ns'>Tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</span>
         </p>
       </div>
       <div className='fl w-100 w-50-ns pv4 ph4' style={{ backgroundColor: colorTwo, color: colorOne }}>
-        <code className='f4 fw6 mb2 db'>{colorTwo}</code>
-        <code className='f5 mb2 db'>{Color(colorTwo).hslString()}</code>
-        <code className='f5 mb2 mb4-ns db'>{Color(colorTwo).rgbString()}</code>
-        <h3 className='f1 f-headline-l mv0'>Aa Bb Cc</h3>
+        <code className='f6 f4-ns fw6 mb2 db'>{colorTwo}</code>
+        <code className='f6 f5-ns mb2 db'>{Color(colorTwo).hslString()}</code>
+        <code className='f6 f5-ns mb2 mb4-ns db'>{Color(colorTwo).rgbString()}</code>
+        <h3 className='f3 f1-m f-headline-l mv0'>Aa Bb Cc</h3>
         <p className='lh-copy measure'>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-          tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.
+          <span className='dn di-ns'>Tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</span>
         </p>
       </div>
     </section>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,10 +15,12 @@ const Footer = () => {
       <p className='mb3 f6 ttu tracked-normal gray'>
         &lt;/&gt; <a href='https://github.com/johnotander/random-a11y' className='link gray dim fw6'>Front End</a> and <a href='https://github.com/johnotander/random-a11y-api' className='link gray dim fw6'>API</a>
       </p>
-      <div className='pt3 cf'>
-  </div>
       <p className='mt5 gray'>
         Related projects: <a href='//jxnblk.com/colorable' title='a11y contrast checker' className='link dim black fw6'>Colorable</a> <a href='//clrs.cc' title='A nicer color palette for the web.' className='dib ml2 link dim black fw6'>Colors</a> <a href='https://github.com/johnotander/random-a11y-combo' title='random a11y combo generator' className='dib ml2 link dim black fw6'>random-a11y-combo</a> <a href='https://github.com/johnotander/get-contrast' title='get contrast ratios and scores' className='dib ml2 link dim black fw6'>get-contrast</a>
+      </p>
+      <h4>What is a11y?</h4>
+      <p className='mt3 measure lh-copy'>
+         Accessibility (a11y) is a measure of a computer system's accessibility is to all people, including those with disabilities or impairments. It concerns both software and hardware and how they are configured in order to enable a disabled or impaired person to use that computer system successfully. Color contrast is just one part of accessibility you should consider.
       </p>
     </footer>
   )
