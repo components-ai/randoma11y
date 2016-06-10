5fb6c296f38ce5d7320fb9491d7a29cffaf78665
Rob Lifford <rob@lifford.org>
2016-06-10 22:34:27 +0200

    add a history page

--- src/store/router.js
+++ src/store/router.js
@@ -3,11 +3,13 @@ import { Route } from 'react-router'
 import App from '../containers/App'
 import Top from '../containers/Top'
 import Explore from '../containers/Explore'
+import Hist from '../containers/History'
 
 const routes = (
   <Route path='/' component={App}>
     <Route path='top' component={Top} />
     <Route path='explore' component={Explore} />
+    <Route path='history' component={Hist} />
   </Route>
 )
 
