8af22f1a042f5fccf279cb16f023f5783a84bc59
John Otander <johnotander@gmail.com>
2016-06-10 08:57:11 -0600

    Fix linting errors

--- src/store/reducers/combos/combos-test.js
+++ src/store/reducers/combos/combos-test.js
@@ -1,16 +1,14 @@
 import test from 'ava'
 import { Map } from 'immutable'
-import isBlank from 'is-blank'
 
 import combosReducer from './'
 
-const combo = ['#fff', '#444']
 let state = combosReducer(undefined, {})
 
 test('initializes with state', t => {
   t.true(Map.isMap(state))
 })
 
-const fireAction = (type, currentState, combo) => (
-  combosReducer(currentState, { type, combo })
-)
+// const fireAction = (type, currentState, combo) => (
+//   combosReducer(currentState, { type, combo })
+// )
