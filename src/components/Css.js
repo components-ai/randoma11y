fd1e6a7dcfbc88676015c874810dff064d75188e
Lachlan Campbell <lachlan.campbell@icloud.com>
2016-07-21 22:48:58 -0400

    copy css to clipboard

--- src/components/Css.js
+++ src/components/Css.js
@@ -2,6 +2,8 @@
 import React, { PropTypes } from 'react'
 import namer from 'color-namer'
 
+import ClipboardButton from './ClipboardButton'
+
 const colorProcess = color => {
   return namer(color).pantone[0].name.replace(/\s/g, '-').toLowerCase()
 }
@@ -18,10 +20,12 @@ const Css = ({ colorOne, colorTwo }) => {
     <div className='w-100 mw6 center'>
       <h3 className='f5 fw6 pb1 mb2 bb b--black-10'>CSS</h3>
       <pre
+        id='css'
         className='code bg-near-white dark-gray br1 mv0 pa2 pre f6 lh-copy'
         style={{ WebkitOverflowScrolling: 'touch' }}
         children={code}
       />
+      <ClipboardButton />
     </div>
   )
 }
