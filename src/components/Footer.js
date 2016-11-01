2a14010c482d8ed06c20331e97547a1450949031
John Otander <johnotander@gmail.com>
2016-11-01 11:20:33 -0600

    Swap out blue for dark-blue for more contrast

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const TwitterButton = () => {
   const tweetText = 'Find a11y friendly color combinations and vote for your favourite at'
   const tweetUrl = `https://twitter.com/share?text=${tweetText}`
   return (
-    <a href={tweetUrl} className='dib ml3-ns fr-ns dim f6 ttu fw6 link mb4 twitter-share-button bg-blue white br1 ph3 pv2' data-via='mrmrs_' data-size='large'>Tweet This</a>
+    <a href={tweetUrl} className='dib ml3-ns fr-ns dim f6 ttu fw6 link mb4 twitter-share-button bg-dark-blue white br1 ph3 pv2' data-via='mrmrs_' data-size='large'>Tweet This</a>
   )
 }
 
@@ -38,7 +38,7 @@ const Footer = () => (
       Related projects: <a href='//jxnblk.com/colorable' title='a11y contrast checker' className='link dim black fw6'>Colorable</a> <a href='//clrs.cc' title='A nicer color palette for the web.' className='dib ml2 link dim black fw6'>Colors</a> <a href='https://github.com/johnotander/random-a11y-combo' title='random a11y combo generator' className='dib ml2 link dim black fw6'>random-a11y-combo</a> <a href='https://github.com/johnotander/get-contrast' title='get contrast ratios and scores' className='dib ml2 link dim black fw6'>get-contrast</a> <a href='//twitter.com/@a11ycolors' title='random a11y twitter bot' className='dib ml2 link dim black fw6'>@a11ycolors</a>
     </p>
     <p className='f5 lh-copy measure mt5'>
-      Want to support randoma11y? Use this <a className='blue link dim' href='https://geo.itunes.apple.com/us/movie/primer/id536457427?at=1l3vqFJ&ct=1l3vqFJ&mt=6' title='Affiliate Link to help support random-a11y.'>
+      Want to support randoma11y? Use this <a className='dark-blue link dim' href='https://geo.itunes.apple.com/us/movie/primer/id536457427?at=1l3vqFJ&ct=1l3vqFJ&mt=6' title='Affiliate Link to help support random-a11y.'>
         affiliate link
       </a> if you are going to buy anything on iTunes or the App Store. It doesn't cost you anything and we get a small cut from the purchase which helps us offset our costs. Thank You.
     </p>
