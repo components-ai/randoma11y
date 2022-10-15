3193239e8320be0d09c012f4eeb4a96ee8882923
John Otander <johnotander@gmail.com>
2022-10-15 11:38:55 -0600

    Add query param for color pinning

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@
 import { jsx } from 'theme-ui'
 import { useEffect, useState } from 'react'
 import contrast from 'get-contrast'
-import { ArrowRightCircle, ArrowUpCircle, ArrowDownCircle, Shuffle } from 'react-feather'
+import { ArrowRightCircle, ArrowUpCircle, ArrowDownCircle } from 'react-feather'
 
 import Layout from '../components/layout'
 import Button from '../components/button'
@@ -11,11 +11,10 @@ import ContrastBoxes from '../components/contrast-boxes'
 
 import { vote, getColorPair } from '../lib'
 
-const Page = () => {
+const Page = ({ pinnedColor }) => {
   const [colorPair, setColorPair] = useState([])
-
   const newColorPair = () => {
-    setColorPair(getColorPair())
+    setColorPair(getColorPair(pinnedColor))
   }
 
   const upvote = async () => {
@@ -29,7 +28,7 @@ const Page = () => {
   const skip = () => {
     newColorPair()
   }
-  const handleKeyUp = e => {
+  const handleKeyUp = (e) => {
     const { key } = e
 
     if (key === 'ArrowUp') {
@@ -67,24 +66,24 @@ const Page = () => {
 
   const contrastRatio = contrast.ratio(colorA, colorB).toFixed(2)
   const contrastScore = contrast.score(colorA, colorB)
-  
+
   return (
     <Layout colorPair={colorPair}>
-<header
+      <header
         sx={{
           display: 'sticky',
           top: 0,
-          borderBottom: "thin solid",
+          borderBottom: 'thin solid',
           display: 'grid',
           alignItems: 'center',
-          gridTemplateColumns: '1fr 1fr 1fr'
+          gridTemplateColumns: '1fr 1fr 1fr',
         }}
       >
         <h1
           sx={{
-            display: "flex",
+            display: 'flex',
             m: 0,
-            alignItems: "center",
+            alignItems: 'center',
             fontSize: [2, 2, 2],
             gap: 3,
             pl: 4,
@@ -93,75 +92,82 @@ const Page = () => {
         >
           <span
             sx={{
-              display: "flex",
-              flexDirection: "column",
+              display: 'flex',
+              flexDirection: 'column',
               justifyContent: 'center',
               height: '24px',
               width: '24px',
-              backgroundImage: 'linear-gradient(90deg, '+colorPair[0]+' 50%, transparent 50%, transparent 100%)',
-              boxShadow: '0 0 0 2px '+colorPair[0],
+              backgroundImage:
+                'linear-gradient(90deg, ' +
+                colorPair[0] +
+                ' 50%, transparent 50%, transparent 100%)',
+              boxShadow: '0 0 0 2px ' + colorPair[0],
               borderRadius: '9999px',
-
-            }}>
-          </span>
+            }}
+          ></span>
           <span sx={{ display: ['none', 'block', 'block'] }}>Randoma11y</span>
         </h1>
-<p
-        sx={{
-          mx: 'auto',
-          textAlign: 'center',
-          fontWeight: 500,
-          fontSize: [0,1,2],
+        <p
+          sx={{
+            mx: 'auto',
+            textAlign: 'center',
+            fontWeight: 500,
+            fontSize: [0, 1, 2],
             fontFamily: 'monospace, monospace',
-        }}
-      >
-        {contrastRatio} <span sx={{display: ['none', 'inline-block', 'inline-block']}}>contrast {contrastScore}</span>
-      </p>
-<div
-        sx={{
-          ml: 'auto',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'stretch',
-        }}
-      >
-        <div sx={{ display: 'none' }}>
-        <Button
-          color={colorB}
-          backgroundColor={colorA}
-          onClick={upvote}
+          }}
         >
-          <ArrowUpCircle size={24} /> Upvote
-        </Button>
+          {contrastRatio}{' '}
+          <span sx={{ display: ['none', 'inline-block', 'inline-block'] }}>
+            contrast {contrastScore}
+          </span>
+        </p>
+        <div
+          sx={{
+            ml: 'auto',
+            height: '100%',
+            display: 'flex',
+            alignItems: 'stretch',
+          }}
+        >
+          <div sx={{ display: 'none' }}>
+            <Button color={colorB} backgroundColor={colorA} onClick={upvote}>
+              <ArrowUpCircle size={24} /> Upvote
+            </Button>
+          </div>
+          <Button
+            color={colorA}
+            backgroundColor={colorB}
+            borderColor={colorB}
+            onClick={skip}
+          >
+            New colors
+            <ArrowRightCircle size={20} />
+          </Button>
+          <div sx={{ display: 'none' }}>
+            <Button
+              color={colorA}
+              backgroundColor={colorB}
+              onClick={downvote}
+              sx={{ ml: 3 }}
+            >
+              <ArrowDownCircle size={24} />
+              Downvote
+            </Button>
+          </div>
         </div>
-<Button
-          color={colorA}
-          backgroundColor={colorB}
-          borderColor={colorB}
-          onClick={skip}
-        >
-          New colors
-          <ArrowRightCircle size={20} />
-        </Button>
-        <div sx={{ display: 'none' }}>
-        <Button
-          color={colorA}
-          backgroundColor={colorB}
-          onClick={downvote}
-          sx={{ ml: 3 }}
-        >
-          <ArrowDownCircle size={24} />
-          Downvote
-        </Button>
-    </div>
-        
-      </div>
       </header>
-      
+
       <ContrastBoxes colorPair={colorPair} />
-      
     </Layout>
   )
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      pinnedColor: context.query.color,
+    },
+  }
 }
 
 export default Page
